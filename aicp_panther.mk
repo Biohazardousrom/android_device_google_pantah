8ddfb74bd4379f5e1aae76c188abc675b27b6e02
duhjoker <biohazardousrom@gmail.com>
2026-03-24 12:34:59 -0500

    add maintainer string

--- aicp_panther.mk
+++ aicp_panther.mk
@@ -31,3 +31,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Duhjoker"
