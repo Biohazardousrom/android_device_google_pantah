88f55bdbc1513e4fc6f0c07f544d304d799e905a
duhjoker <biohazardousrom@gmail.com>
2026-06-21 12:32:15 -0500

    Alphadroid bringup
    bring up lineage device trees to alphadroid

---
+++ alpha_cheetah.mk
@@ -0,0 +1,79 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build config
+
+# append time of day to zip
+ALPHA_VERSION_APPEND_TIME_OF_DAY := false
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# (valid only for GAPPS builds)
+TARGET_INCLUDE_GOOGLE_COMMS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+WITH_ADB_INSECURE := false
+
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := duhjoker
+
+WITH_GMS := true
+
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+# include pixel parts
+include packages/apps/PixelParts/device.mk
+
+-include vendor/lineage-priv/keys/keys.mk
+
+# Inherit some common stuff
+TARGET_DISABLE_EPPE := true
+
+# Inherit some common stuff
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := cheetah
+DEVICE_PATH := device/google/pantah
+VENDOR_PATH := vendor/google/cheetah
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 7 Pro
+PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="cheetah-user 16 BP4A.251205.006 14401865 release-keys" \
+    BuildFingerprint=google/cheetah/cheetah:16/BP4A.251205.006/14401865:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
---
+++ alpha_panther.mk
@@ -0,0 +1,83 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build config
+
+# append time of day to zip
+ALPHA_VERSION_APPEND_TIME_OF_DAY := false
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# (valid only for GAPPS builds)
+TARGET_INCLUDE_GOOGLE_COMMS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+WITH_ADB_INSECURE := false
+
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := duhjoker
+
+WITH_GMS := true
+
+WITH_PIXEL_OVERLAYS := true
+
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+# include pixel parts
+include packages/apps/PixelParts/device.mk
+
+-include vendor/lineage-priv/keys/keys.mk
+
+#-include vendor/pixeloverlays/panther/panther-vendor.mk
+#-include vendor/pixeloverlays/gms/gms-vendor.mk
+
+# Inherit some common stuff
+TARGET_DISABLE_EPPE := true
+
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := panther
+DEVICE_PATH := device/google/pantah
+VENDOR_PATH := vendor/google/panther
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 7
+PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="panther-user 16 BP4A.251205.006 14401865 release-keys" \
+    BuildFingerprint=google/panther/panther:16/BP4A.251205.006/14401865:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
